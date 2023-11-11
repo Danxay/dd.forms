cedb4a53ae68855f5f1c4a756607be840afdfe40
Danxay <dan.dryagin@gmail.com>
2023-11-11 21:15:53 +0600

    fix images

--- frontend/src/components/CardWithoutBanner/CardWithoutBanner.jsx
+++ frontend/src/components/CardWithoutBanner/CardWithoutBanner.jsx
@@ -8,7 +8,8 @@ import cn from 'classnames'
 export const CardWithoutBanner = ({data}) => {
   return (
     <article className={styles.wrapper}>
-      <Image src={data.image} width={150} height={150} alt="Аватарка" className={styles.image}/>
+      {/*<Image src={data.image} width={150} height={150} alt="Аватарка" className={styles.image}/>*/}
+      <img src={data.image} alt="Аватарка" className={styles.image}/>
       <h1 className={styles.name}>{data.fullname}</h1>
       <p className={styles.bio}>{data.bio}</p>
       <div className={styles.links}>
