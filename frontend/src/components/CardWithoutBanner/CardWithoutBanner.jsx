fd2d393c080f86da66443c599aa729e7f3cf3497
Danxay <dan.dryagin@gmail.com>
2023-11-11 20:39:08 +0600

    change url

--- frontend/src/components/CardWithoutBanner/CardWithoutBanner.jsx
+++ frontend/src/components/CardWithoutBanner/CardWithoutBanner.jsx
@@ -8,7 +8,7 @@ import cn from 'classnames'
 export const CardWithoutBanner = ({data}) => {
   return (
     <article className={styles.wrapper}>
-      <img src={data.image} alt="Аватарка" className={styles.image} />
+      <Image src={data.image} width={150} height={150} alt="Аватарка" className={styles.image}/>
       <h1 className={styles.name}>{data.fullname}</h1>
       <p className={styles.bio}>{data.bio}</p>
       <div className={styles.links}>
