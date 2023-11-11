d4088908f92707dee7139337ace8402f7e700c80
Ilian445 <ty4dg7@gmail.com>
2023-11-11 23:10:24 +0600

    Last one

--- backend/main.py
+++ backend/main.py
@@ -22,25 +22,20 @@ app.mount("/images", StaticFiles(directory="../bot/photo"), name="images")
 
 @app.get("/card/{id}")
 async def get_data(id: int):
-    while True:
-        try:
-            con = sqlite3.connect("../bot/data.db")
-            cursor = con.cursor()
-            cursor.execute(f"SELECT * FROM data WHERE id = {id}")
-            result = cursor.fetchone()
-            response = {
-                'id': result[0],
-                'fullname': result[1],
-                'telegram': result[2],
-                'vk': result[3],
-                'contact': result[4],
-                'type': result[5],
-                'bio': result[6],
-            }
-            con.close()
-            return response
-            
-        except:
-            pass
+    con = sqlite3.connect("../bot/data.db")
+    cursor = con.cursor()
+    cursor.execute(f"SELECT * FROM data WHERE id = {id}")
+    result = cursor.fetchone()
+    response = {
+        'id': result[0],
+        'fullname': result[1],
+        'telegram': result[2],
+        'vk': result[3],
+        'contact': result[4],
+        'type': result[5],
+        'bio': result[6],
+    }
+    con.close()
+    return response
 
 # uvicorn main:app --host 0.0.0.0 --port 8888
