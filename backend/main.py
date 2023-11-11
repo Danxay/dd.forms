dea931d6e4a81642f6f93c199906ef2f54d65c53
Ilian445 <ty4dg7@gmail.com>
2023-11-11 22:48:08 +0600

    Fix backend

--- backend/main.py
+++ backend/main.py
@@ -18,24 +18,27 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-con = sqlite3.connect("../bot/data.db")
-cursor = con.cursor()
-
 app.mount("/images", StaticFiles(directory="../bot/photo"), name="images")
 
 @app.get("/card/{id}")
 async def get_data(id: int):
-    cursor.execute(f"SELECT * FROM data WHERE id = {id}")
-    result = cursor.fetchone()
-    response = {
-        'id': result[0],
-        'fullname': result[1],
-        'telegram': result[2],
-        'vk': result[3],
-        'contact': result[4],
-        'type': result[5],
-        'bio': result[6],
-    }
-    return response
+    while True:
+    try:
+        con = sqlite3.connect("../bot/data.db")
+        cursor = con.cursor()
+        cursor.execute(f"SELECT * FROM data WHERE id = {id}")
+        result = cursor.fetchone()
+        response = {
+            'id': result[0],
+            'fullname': result[1],
+            'telegram': result[2],
+            'vk': result[3],
+            'contact': result[4],
+            'type': result[5],
+            'bio': result[6],
+        }
+        con.close()
+        return response
+        break
 
 # uvicorn main:app --host 0.0.0.0 --port 8888
