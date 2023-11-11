750e4ac83b11eaf39453d78302118d10a320b327
Ilian445 <ty4dg7@gmail.com>
2023-11-11 22:58:56 +0600

    f

--- backend/main.py
+++ backend/main.py
@@ -2,52 +2,43 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 import sqlite3
-import uvicorn
 
+app = FastAPI()
 
+origins = [
+    "https://dd-forms-danxay.vercel.app",
+    "https://dd-forms.vercel.app",
+]
 
-class App:
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-    app = FastAPI()
+app.mount("/images", StaticFiles(directory="../bot/photo"), name="images")
 
-    origins = [
-        "https://dd-forms-danxay.vercel.app",
-        "https://dd-forms.vercel.app",
-    ]
-
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=origins,
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
-
-    app.mount("/images", StaticFiles(directory="../bot/photo"), name="images")
-
-    @app.get("/card/{id}")
-    async def get_data(id: int):
-        while True:
-            try:
-                con = sqlite3.connect("../bot/data.db")
-                cursor = con.cursor()
-                cursor.execute(f"SELECT * FROM data WHERE id = {id}")
-                result = cursor.fetchone()
-                response = {
-                    'id': result[0],
-                    'fullname': result[1],
-                    'telegram': result[2],
-                    'vk': result[3],
-                    'contact': result[4],
-                    'type': result[5],
-                    'bio': result[6],
-                }
-                con.close()
-                return response
-                break
-
-app = App()
-
-uvicorn.run("main:app", host="0.0.0.0", port=300, log_level="info")
+@app.get("/card/{id}")
+async def get_data(id: int):
+    while True:
+        try:
+            con = sqlite3.connect("../bot/data.db")
+            cursor = con.cursor()
+            cursor.execute(f"SELECT * FROM data WHERE id = {id}")
+            result = cursor.fetchone()
+            response = {
+                'id': result[0],
+                'fullname': result[1],
+                'telegram': result[2],
+                'vk': result[3],
+                'contact': result[4],
+                'type': result[5],
+                'bio': result[6],
+            }
+            con.close()
+            return response
+            break
 
 # uvicorn main:app --host 0.0.0.0 --port 8888
